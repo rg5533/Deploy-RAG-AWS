83e7bef27d959a0508fcb89e84d811fc4dfd46ff
Rohit Gupta <rohitgupta5533@gmail.com>
2024-09-29 10:37:43 +1000

    adding creating ingestion and retrieval file and creating app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import json
+import sys
+import boto3
+import streamlit as st
+
+
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain.llms.bedrock import Bedrock
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA    
+
+from langchain.vectorstores import FAISS
+
+
+
+
+
+
+
+
+
+
+
